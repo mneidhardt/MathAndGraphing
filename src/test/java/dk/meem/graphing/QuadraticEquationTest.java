4cdf8c4b895d4c4ec9009b93fb04041c9fabed28
Michael Neidhardt <mneidhardt@gmail.com>
2016-10-17 19:17:52 +0200

    Moved test to src/test/java.

--- src/test/java/dk/meem/graphing/QuadraticEquationTest.java
+++ src/test/java/dk/meem/graphing/QuadraticEquationTest.java
@@ -5,41 +5,32 @@ import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
 public class QuadraticEquationTest {
-	@Rule
-	public ExpectedException thrown= ExpectedException.none();
 
-	@Test
+	@Test(expected=IOException.class)
 	public void testQuadraticEquation1() throws IOException {
     	double[] coeff = {0.00001, 0.00002, 0.00003, 0.0004};
-
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
-	    thrown.expect(IOException.class);
-        throw new IOException();
 	}
 
-	@Test
+	@Test(expected=IOException.class)
 	public void testQuadraticEquation2() throws IOException {
     	double[] coeff = {0.00003, 0.0004};
-
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
-	    thrown.expect(IOException.class);
-        throw new IOException();
 	}
 
-	@Test
+	@Test(expected=IOException.class)
 	public void testQuadraticEquation3() throws IOException {
     	double[] coeff = {0.0, -5, 15};
-
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
-	    thrown.expect(IOException.class);
-        throw new IOException();
 	}
 
 	@Test
@@ -47,18 +38,14 @@ public class QuadraticEquationTest {
     	double[] coeff = {0.00001, 0.00002, 0.00003};
 
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
-	    thrown.expect(IOException.class);
-        throw new IOException();
 	}
 
 	@Test
 	public void testSolve1() throws IOException {
-		double[] coeff = {6, -5, 1};
+		double[] coeff = {1, -2, 2};
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
     	
-    	List<Double> roots = new ArrayList<Double>();
-    	roots.add(1.0/3.0);
-    	roots.add(1.0/2.0);
+    	Set<Double> roots = new HashSet<Double>();
     	assertEquals(roots, qe.solve());
 	}
 	
@@ -67,28 +54,9 @@ public class QuadraticEquationTest {
 		double[] coeff = {6, -5, 1};
     	QuadraticEquation qe = new QuadraticEquation(200, 100, coeff);
     	
-    	List<Double> roots = new ArrayList<Double>();
+    	Set<Double> roots = new HashSet<Double>();
     	roots.add(1.0/3.0);
     	roots.add(1.0/2.0);
     	assertEquals(roots, qe.solve());
-		
-
-		fail("Not yet implemented");
 	}
-
-	@Test
-	public void testGetEquation() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetD() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetRoots() {
-		fail("Not yet implemented");
-	}
-
 }
